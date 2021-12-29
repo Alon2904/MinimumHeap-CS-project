aaa0dbdd8cf2d63a4a9745f2cf29347406c0ad50
[Alon Cohen] <[aloncohen2904@gmail.com]>
2021-12-29 23:41:01 +0000

    Erased the switch that handeled MakeHeap and transfered it to InputParser

--- main.java
+++ main.java
@@ -22,73 +22,63 @@ public class main {
         System.out.println("A - Sorted lists");
         System.out.println("B - Unsorted Lists");
         System.out.println("C - Unsorted and foreign lists");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("");
 
         //recieving the heap type wer'e working with
         String status = scanner.nextLine();
         
         HeapLinkedList[] arrOfLists = new HeapLinkedList[100];
-       
-       
-
-                    String currLine;
-                    int index = -1; //index of current linkedlist
+        String currLine;
+        int index = -1; //index of current linkedlist
                     
+        InputParser parser = new InputParser();
                     
         while(scanner.hasNext()){
-           currLine = scanner.nextLine();
+            parser.parse(scanner.nextLine(),status,index,arrOfLists);
+        
 
-           // TODO - add a function to make it look nicer
-           if(currLine.equals("MakeHeap")){
-            index++;
-            HeapLinkedList list;
-            switch(status) {
-                case "A":
-                list = new sortedLists();
-                System.out.println("A");
-                
-
-                case "B":
-                list = new unsortedLists();
-                System.out.println("B");
-                
-
-                case "C":
-                list = new foreignUnsortedLists();
-                System.out.println("C");
-                
-
-            
-
-            
-            arrOfLists[index] = list;
-           System.out.println("input - MakeHeap");
-           System.out.println(arrOfLists[index]);
         }
-    }
-
-    while(scanner.hasNext()){
-        currLine = scanner.nextLine();
-        // TODO - create a function that will take care of merge, insert,min,extraxtMin
-    }
-
-}
-
-
            
 
+        //    // TODO - add a function to make it look nicer
+        //    if(currLine.equals("MakeHeap")){
+        //     index++;
+        //     HeapLinkedList list;
+        //     switch(status) {
+        //         case "A":
+        //         list = new sortedLists();
+        //         System.out.println("A");
+        //         break;
+                
 
+        //         case "B":
+        //         list = new unsortedLists();
+        //         System.out.println("B");
+        //         break;
+                
 
-           
+        //         case "C":
+        //         list = new foreignUnsortedLists();
+        //         System.out.println("C");
+        //         break;
+                
+
+            
+
+            
+        //     arrOfLists[index] = list;
+        //    System.out.println("input - MakeHeap");
+        //    System.out.println(arrOfLists[index]);
+        // }
+    
+
+ 
+
+}       
 
 
         
-       
-          
-}
-public void analyze(Heap list,String str){
-
-
-}
 }
 
 
