4a8a9aab00bac30101cc075d5df90927e50a7b98
[Alon Cohen] <[aloncohen2904@gmail.com]>
2021-12-31 00:39:13 +0000

    the whole file has been commented - there is no need for the file

--- BaseLinkedList.java
+++ BaseLinkedList.java
@@ -1,60 +1,60 @@
-class BaseLinkedList {
-    Node first;
-    Node last;
+// class BaseLinkedList {
+//     Node first;
+//     Node last;
  
-    class Node {
-        int value;
-        Node next;
-        Node parent;
-        Node leftChild;
-        Node rightChild;
+//     class Node {
+//         int value;
+//         Node next;
+//         Node parent;
+//         Node leftChild;
+//         Node rightChild;
 
-        Node(int value) {
-             this.value = value; 
-            }
+//         Node(int value) {
+//              this.value = value; 
+//             }
     
-        }
-    public void add(int value) {
-        Node node = new Node(value);
-        node.next = null;
+//         }
+//     public void add(int value) {
+//         Node node = new Node(value);
+//         node.next = null;
 
-        if (this.first == null) {
-            this.first = node;
-            this.last = node;
-        } else {
-            this.last.next = node;
-            this.last = node;
-        }
-    }
+//         if (this.first == null) {
+//             this.first = node;
+//             this.last = node;
+//         } else {
+//             this.last.next = node;
+//             this.last = node;
+//         }
+//     }
 
-    public void delete(int index) {
-        Node current = this.first;
-        Node previous = null;
+//     public void delete(int index) {
+//         Node current = this.first;
+//         Node previous = null;
 
-        if (index == 0 && current != null) {
-            this.first = current.next;
-        } else {
-            int counter = 0;
+//         if (index == 0 && current != null) {
+//             this.first = current.next;
+//         } else {
+//             int counter = 0;
 
-            while (current != null) {
+//             while (current != null) {
  
-                if (counter == index) {
-                    previous.next = current.next;
-                    break;
-                } else {
-                    previous = current;
-                    current = current.next;
-                    counter++;
-                }
-            }
+//                 if (counter == index) {
+//                     previous.next = current.next;
+//                     break;
+//                 } else {
+//                     previous = current;
+//                     current = current.next;
+//                     counter++;
+//                 }
+//             }
 
-            if (current == null) {
-                throw new IndexOutOfBoundsException("Can't delete as an item does not exist at this index");
-            }
-        }
-    }
+//             if (current == null) {
+//                 throw new IndexOutOfBoundsException("Can't delete as an item does not exist at this index");
+//             }
+//         }
+//     }
 
    
 
  
-}
+// }
